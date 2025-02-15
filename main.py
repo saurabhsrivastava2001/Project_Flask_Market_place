2b909f20b4d2d860a6b7a86c6148680798deeb0c
saurabhsrivastava2001 <srisaurabh2001@gmail.com>
2025-02-15 16:15:27 +0530

    create database

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db'
+
+db=SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id=db.Column(db.Integer(),primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False , unique=True)
+    price= db.Column(db.Integer(),nullable=False)
+    barcode= db.Column(db.String(length=12),nullable=False,unique=True)
+    description=db.Column(db.String(length=1024),nullable=False,unique=True)
+
+    def __repr__(self):
+        return f'Item {self.name}'
 
 @app.route('/')
 @app.route('/home')
@@ -8,11 +23,7 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    items = [
-        {'id': 1, 'name': 'nithis', 'barcode': '1248r9223','marks': 95},
-        {'id': 2, 'name': 'yash', 'barcode': '123985473165', 'marks': 50},
-        {'id': 3, 'name': 'saurabh', 'barcode': '231985128446', 'marks': 150}
-    ]
+    items = Item.query.all()
     return render_template('market.html', items=items)
 
 if __name__=="__main__":

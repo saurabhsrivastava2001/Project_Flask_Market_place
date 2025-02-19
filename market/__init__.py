16a075f940eed908fa108e32dbb70a223746802b
saurabhsrivastava2001 <srisaurabh2001@gmail.com>
2025-02-20 01:16:12 +0530

    Project restructure

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db'
 
+app.config['SECRET_KEY'] = '15efda52ac348a4f1c70d30d'
+
 db=SQLAlchemy(app)
 
 
--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,8 @@
-from market import app
-from flask import render_template
-from market.model import Item
+from market import app,db
+from flask import render_template , redirect,url_for
+from market.model import Item,User
+from market.froms import RegisterForm
+
 @app.route('/')
 @app.route('/home')
 def home_page():
@@ -10,3 +12,15 @@ def home_page():
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
+
+@app.route('/register',methods=['GET','POST'])
+def register_page():
+    form=RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username= form.username.data,emailid=form.emailid.data,password_hash=form.password1.data,budget=1000)
+        with app.app_context():
+            db.session.add(user_to_create)
+            db.session.commit()
+        return redirect(url_for('market_page'))
+
+    return render_template('register.html',form=form)
